e07129fa6d6a44eb0003e01f4741b6ac32247e6b
user <vaishnavipagire123@gmail.com>
2026-07-28 17:15:11 +0530

    Added Payment Pages

---
+++ src/utils/storage.js
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const STORAGE_KEYS = {
+  USER: 'USER',
+  LOGIN: 'LOGIN',
+};
+
+export const saveUser = async user => {
+  await AsyncStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
+};
+
+export const getUser = async () => {
+  const user = await AsyncStorage.getItem(STORAGE_KEYS.USER);
+  return user ? JSON.parse(user) : null;
+};
+
+export const loginUser = async () => {
+  await AsyncStorage.setItem(STORAGE_KEYS.LOGIN, 'true');
+};
+
+export const logoutUser = async () => {
+  await AsyncStorage.removeItem(STORAGE_KEYS.LOGIN);
+};
+
+export const isLoggedIn = async () => {
+  const value = await AsyncStorage.getItem(STORAGE_KEYS.LOGIN);
+  return value === 'true';
+};
